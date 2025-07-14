3bd457f50e46d0b9a26554d0b8ed4b305100bb6e
Arthur <Arthur.fabricyo@gmail.com>
2025-07-13 23:27:39 -0300

    Adicionando colors e configutando o add.json

--- android/app/src/main/java/com/fabricyo/target/MainActivity.kt
+++ android/app/src/main/java/com/fabricyo/target/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.fabricyo.target
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/fabricyo/target/MainApplication.kt
+++ android/app/src/main/java/com/fabricyo/target/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.fabricyo.target
 
 import android.app.Application
 import android.content.res.Configuration
