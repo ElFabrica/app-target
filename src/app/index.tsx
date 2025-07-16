910e22653ea6deb7891639053f3d572813e51415
Arthur <Arthur.fabricyo@gmail.com>
2025-07-15 23:32:03 -0300

    Adicionando o separator e estilizando o header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,15 @@ import { Text, View, Button } from "react-native";
 import { router } from "expo-router";
 import { HomeHeader } from "@/componentes/HomeHeader";
 
+
 export default function Index() {
     return(
         <View>
-            <HomeHeader/>
+            <HomeHeader
+            data={{total: "R$ 2.680,00"}}
+            />
+            
+        
         </View>
     )
 }
--- src/componentes/HomeHeader/index.tsx
+++ src/componentes/HomeHeader/index.tsx
@@ -2,14 +2,34 @@ import {LinearGradient} from "expo-linear-gradient"
 
 import { styles } from "./styles"
 import { colors } from "@/theme/colors"
+import { Text, View } from "react-native"
+import { Separator } from "../Separator"
 
-export function HomeHeader(){
+export type homeHeaderProps ={
+     total: string
+}
+
+type props ={
+    data: homeHeaderProps
+}
+
+export function HomeHeader({data}: props){
     return(
         <LinearGradient 
         colors={[colors.blue[500], colors.blue[800]]}
         style={styles.container}
         >
+            <View>
+                <Text style={styles.label}>
+                    Total que você possui
+                </Text>
+                <Text style={styles.total}>{data.total}</Text>
+                
 
+            </View>
+            <Separator
+                color={colors.blue[400]}
+                />
         </LinearGradient>
 
     )
