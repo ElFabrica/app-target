bf30119a06fc84fd94e3739bec4f56b08a676c36
Arthur <Arthur.fabricyo@gmail.com>
2025-07-16 23:51:19 -0300

    Adcionando componentes de lista e de target

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,42 @@
 import { Text, View, Button } from "react-native";
-import { router } from "expo-router";
-import { HomeHeader } from "@/componentes/HomeHeader";
 
+import { List } from "@/componentes/List";
+import { HomeHeader } from "@/componentes/HomeHeader";
+import { Target } from "@/componentes/Target";
 
 export default function Index() {
 
-    const Summary ={
+    const Summary = {
         total: "R$ 2.680,00",
-        input: {label:"Entradas", value: "R$ 6,184.90" },
-        output:  {label:"Saídas", value: "-R$ 883.65" }
+        input: { label: "Entradas", value: "R$ 6,184.90" },
+        output: { label: "Saídas", value: "-R$ 883.65" },
+        
     }
+    const targets = [
+        {
+            id: "",
+            current: "200",
+            name: "O caba lá",
+            percentage: "10",
+            target: "10"
+        }
+    ]
 
-    return(
+    return (
         <View>
             <HomeHeader
-            data={Summary}
+                data={Summary}
             />
             
-        
+            <List
+            data={targets}
+            renderItem={({item}) => <Target data={item}/> }
+            keyExtractor={(item) => item.id}
+            title="Metas"
+            emptyMessage="Nenhuma meta. Toque em nova meta para adicionar"
+            containerStyle={{paddingHorizontal: 24}}
+            />
+
         </View>
     )
 }
