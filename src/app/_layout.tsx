a2ac464e24346727fa4b05bf62036799ea005a22
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-07-28 13:29:49 -0300

    Adicionando suspense no inicio da aplicação

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,40 +1,47 @@
-import { Tabs, Stack } from "expo-router";
+import { Suspense } from "react";
+
+import { Stack } from "expo-router";
 import {
     useFonts,
     Inter_400Regular,
     Inter_500Medium,
     Inter_700Bold
 } from "@expo-google-fonts/inter"
+
+import { SQLiteProvider } from "expo-sqlite"
+
+import { migrate } from "@/database/migrate";
+
 import { Loading } from "@/componentes/Loading";
+import { colors } from "@/theme";
 
 
-export default function Layout(){
+export default function Layout() {
 
     const [fontsLoades] = useFonts({
         Inter_400Regular,
         Inter_500Medium,
         Inter_700Bold
-    })  
+    })
 
-    if (!fontsLoades){
-        return <Loading/>
+    if (!fontsLoades) {
+        return <Loading />
     }
 
     return (
-    
-
-<Stack >
-    
-<Stack.Screen
-name="index"
-
-options={{
-    title: "Home",
-    
-}}
-/>
-</Stack>
-
-)
+        <Suspense fallback={<Loading />}>
+        <SQLiteProvider
+        databaseName="target.db"
+        onInit={migrate}
+        useSuspense
+        >
+            <Stack screenOptions={{
+                headerShown: false,
+                contentStyle: { backgroundColor: colors.white }
+                }}>
+            </Stack>
+        </SQLiteProvider>
+        </Suspense>
+    )
 
 }
